2933b589c0678844455df21865329d16792d8dfa
Nofaldi Fikrul Atmam <nofaldi.atmam@shopee.com>
2024-05-12 21:17:56 +0700

    adjust mobile slider

--- src/components/react/ProductList.tsx
+++ src/components/react/ProductList.tsx
@@ -27,17 +27,14 @@ export default function ProductList() {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
+          slidesToScroll: 1,
         },
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
+          slidesToScroll: 1,
         },
       },
       {
@@ -67,7 +64,7 @@ export default function ProductList() {
       <Slider {...settings}>
         {logos.map((logo) => (
           <div className="h-64 w-64 items-center p-4">
-            <div className="flex h-full w-full items-center">
+            <div className="flex h-full w-full items-center justify-center">
               <img src={logo.src} alt={logo.alt} />
             </div>
           </div>
