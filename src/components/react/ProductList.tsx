bd9bab33f693065b8eb8647ad46704cd7c24876e
Nofaldi Fikrul Atmam <nofaldi.atmam@shopee.com>
2024-05-26 19:11:24 +0700

    add hero image slider

--- src/components/react/ProductList.tsx
+++ src/components/react/ProductList.tsx
@@ -1,24 +1,28 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import shunda from "@images/product/shunda.png";
-import ecofon from "@images/product/ecofon.png";
-import duma from "@images/product/duma.png";
-import alfafon from "@images/product/alfafon.png";
-import batikplafon from "@images/product/batikplafon.png";
-import envara from "@images/product/envara.png";
-import janur from "@images/product/janur.png";
-import pvcplank from "@images/product/pvcplank.png";
-import petrafone from "@images/product/petrafone.png";
 
-export default function ProductList() {
+export interface ProductImage {
+  src: string;
+  alt: string;
+}
+
+export interface ProductListProps {
+  products: ProductImage[];
+  slideToShow: number;
+}
+
+export default function ProductList({
+  products,
+  slideToShow,
+}: ProductListProps) {
   const settings = {
     infinite: true,
     speed: 1000,
     autoplay: true,
     autoplaySpeed: 2000,
     cssEase: "linear",
-    slidesToShow: 5,
+    slidesToShow: slideToShow,
     slidesToScroll: 1,
     initialSlide: 0,
     className: "h-64",
@@ -47,22 +51,10 @@ export default function ProductList() {
     ],
   };
 
-  const logos = [
-    { src: shunda.src, alt: "shunda" },
-    { src: ecofon.src, alt: "ecofon" },
-    { src: duma.src, alt: "duma" },
-    { src: alfafon.src, alt: "alfafon" },
-    { src: batikplafon.src, alt: "batik plafon" },
-    { src: envara.src, alt: "envara" },
-    { src: janur.src, alt: "janur" },
-    { src: pvcplank.src, alt: "pvcplank" },
-    { src: petrafone.src, alt: "petrafone" },
-  ];
-
   return (
     <div className="slider-container">
       <Slider {...settings}>
-        {logos.map((logo) => (
+        {products.map((logo) => (
           <div className="h-64 w-64 items-center p-4">
             <div className="flex h-full w-full items-center justify-center">
               <img src={logo.src} alt={logo.alt} />
--- src/data_files/constants.ts
+++ src/data_files/constants.ts
@@ -2,13 +2,11 @@ import ogImageSrc from "@images/social.png";
 
 export const SITE = {
   title: "SUMBER PLAFON",
-  tagline: "Top-quality Hardware Tools",
-  description:
-    "ScrewFast offers top-tier hardware tools and expert construction services to meet all your project needs. Start exploring and contact our sales team for superior quality and reliability.",
-  description_short:
-    "ScrewFast offers top-tier hardware tools and expert construction services to meet all your project needs.",
+  tagline: "Pusat Produk PVC dengan merek merek terpercaya",
+  description: "Pusat Produk PVC dengan merek merek terpercaya",
+  description_short: "Pusat Produk PVC dengan merek merek terpercaya",
   url: "https://screwfast.uk",
-  author: "Emil Gulamov",
+  author: "OFMX",
 };
 
 export const SEO = {
